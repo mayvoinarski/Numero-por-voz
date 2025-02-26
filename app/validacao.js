08800ad62340c7f6d8a4127213d9f11b8386a00c
Maysa Voinarski <mayvoinarski@gmail.com>
2025-02-26 17:42:40 -0300

    Ajuste botão

--- app/validacao.js
+++ app/validacao.js
@@ -14,7 +14,9 @@ function guessIsRight(guess) {
 
     if (number === secretNumber) {
         document.body.innerHTML = `<h2> Você acertou! </h2>
-        <h3> o número secreto era ${secretNumber}`
+        <h3> o número secreto era ${secretNumber} <h3>
+        <button id="play-again" class="btn-play"> Jogar novamente </button>
+        `
     }   
     else if (number > secretNumber) {
         guessElement.innerHTML += `<div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>`
@@ -30,3 +32,9 @@ function invalidGuess (number) {
 function guessMinorOrBigger (number) {
     return number > biggerValue || number < minorValue 
 }
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == "play-again") {
+        window.location.reload()
+    }
+})
